2a725d77ac1355a592044907a19a5a9823e780a9
Michael-Costello <Michael-Costello@5CG5031V9P.na.odcorp.net>
2017-10-19 18:06:20 -0400

    committing product affinity as guideline for collab purposes

--- personalization-web/src/main/java/com/officedepot/eai/personalization/productaffinity/ProductAffinityService.java
+++ personalization-web/src/main/java/com/officedepot/eai/personalization/productaffinity/ProductAffinityService.java
@@ -8,7 +8,23 @@ public class ProductAffinityService extends RouteBuilder{
 
 	@Override
 	public void configure() throws Exception {
+		
+		/**
+		 * @author Michael-Costello
+		 * FIXME this needs to be flushed out 
+		 */
+		//onException(SQLException).transform("the default response");
 		// TODO Auto-generated method stub
+		/**
+		 * @author Michael-Costello
+		 * TODO mcostell
+		 * need to wire up camel-jdbc 
+		 */
+		from("jdbc:aopsDS?customerAffinityPS")
+		.transform(simple("this should be a copybook xform"))
+		//.convertBodyTo(CustomerAffinty.class) //FIXME craete customer affinty 
+		;
+		
 		
 	}
 
